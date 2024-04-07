ad5e58309d9262614710e1d163330dd829d0399b
Mauch25 <chrzanowskimateusz25@gmail.com>
2024-04-07 14:59:13 +0200

    Zad4

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,22 @@ import geometry.*;
     public class Main {
         public static void main(String[] args) {
 
+            Circle[] circles = new Circle[3];
+            circles[0] = new Circle(new Point(0, 0), 2.0);
+            circles[1] = new ColoredCircle(new Point(3, 4), 3.0, "red");
+            circles[2] = new ColoredCircle(new Point(-2, -2), 1.5, "green");
+    
+           
+            for (Circle circle : circles) {
+                double area = circle.getArea();
+                System.out.println("Pole powierzchni koła: " + area);
+    
+              
+                if (circle instanceof ColoredCircle) {
+                    String color = ((ColoredCircle) circle).getColor();
+                    System.out.println("Kolor koła: " + color);
+                }
+
             Point center = new Point(3.0, 4.0);
     
             
@@ -17,6 +33,6 @@ import geometry.*;
 
 
         }
-    }
-
+    } 
+ }
 
